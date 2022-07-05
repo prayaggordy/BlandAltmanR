6d157a26b2994efbb4230e968dfe84a1ef57c842
Prayag Gordy <prayaggordy@gmail.com>
2022-07-05 18:04:36 -0400

    remove fn

--- R/individual_diffs.R
+++
@@ -1,6 +0,0 @@
-individual_diffs <- function(df, measures = NULL, names_sep = "_") {
-	df %>%
-		tidyr::pivot_longer(cols = dplyr::starts_with(measures),
-												names_to = c("measure", ".value"),
-												names_sep = names_sep)
-}
